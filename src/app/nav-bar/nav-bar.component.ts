6c0657e1df84e8e9f9de2ad7579d7b82f8c35c7c
Maxime Chevalier <chevalier.maxime@outlook.com>
2018-10-16 11:51:05 +0200

    #3 Navbar improvements

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
+import { Observable, timer } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import { UserService } from '../user.service';
+import { MatSidenav } from '@angular/material';
 
 @Component({
   selector: 'app-nav-bar',
@@ -11,24 +12,51 @@ import { UserService } from '../user.service';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent {
-  errorMessage = '';
+  @ViewChild('drawer') public sidenav: MatSidenav;
+  errorMessage = null;
+  successMessage = null;
+  fadeOutTimer = null;
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
   constructor(private router: Router, private breakpointObserver: BreakpointObserver,
-    private userservice: UserService) {}
+    public userservice: UserService) {}
 
   logout() {
+    // close();
     this.userservice.logout().subscribe(
-      () => this.router.navigate(['/']),
-      () => this.errorMessage = 'Error while login out'
+      () => {
+        this.successMessage = 'Logged out with succes';
+        this.router.navigate(['login']);
+      },
+      () => {
+        this.errorMessage = 'Error while login out';
+      }
+    );
+    this.fadeOutTimer = timer(3000);
+    const fadeOutMessages = this.fadeOutTimer.subscribe(
+      () => {
+        console.log('fadeOut');
+        this.errorMessage = null;
+        this.successMessage = null;
+      }
     );
   }
 
   isAdmin(): boolean {
     return this.userservice.isAdmin();
+  }
+
+  close(): void {
+    this.isHandset$.subscribe(
+      (isHandset) => {
+        if (isHandset) {
+          this.sidenav.close();
+        }
+      }
+    );
   }
 
   }
