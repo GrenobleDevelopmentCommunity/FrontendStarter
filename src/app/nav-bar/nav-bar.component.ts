496dd60bd52a35534a1d7eca577538a1159a42f6
Maxime Chevalier <chevalier.maxime@outlook.com>
2018-10-18 14:50:54 +0200

    #5 Adding cards

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,5 +1,5 @@
-import { Component, ViewChild, ChangeDetectorRef, OnDestroy } from '@angular/core';
-import { BreakpointObserver, Breakpoints, BreakpointState, MediaMatcher } from '@angular/cdk/layout';
+import { Component, ViewChild } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable, timer } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Router } from '@angular/router';
@@ -11,31 +11,20 @@ import { MatSidenav } from '@angular/material';
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent implements OnDestroy {
+export class NavBarComponent {
   @ViewChild('drawer') public sidenav: MatSidenav;
   errorMessage = null;
   successMessage = null;
   fadeOutTimer = null;
 
-  mobileQuery: MediaQueryList;
-  private _mobileQueryListener: () => void;
 
-
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.HandsetPortrait)
     .pipe(
       map(result => result.matches)
     );
 
   constructor(private router: Router, private breakpointObserver: BreakpointObserver,
-    public userservice: UserService, changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
-      this.mobileQuery = media.matchMedia('(max-width: 600px)');
-      this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-      this.mobileQuery.addListener(this._mobileQueryListener);
-    }
-
-    ngOnDestroy(): void {
-      this.mobileQuery.removeListener(this._mobileQueryListener);
-    }
+    public userservice: UserService) {}
 
   logout() {
     this.close();
